057c782a706a65a218b4a7293b0ae6376aa5effe
Lukáš Veškrna <lukas.veskrna@gmail.com>
2023-02-26 18:07:47 +0100

    Design & models

--- langtool/settings.py
+++ langtool/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
+
+AUTH_USER_MODEL = "learn.User"
+
+# App-specific config
+
+INITIAL_INTERVAL = (0.25, 4) # (failure, success) in hours
--- learn/models.py
+++ learn/models.py
@@ -1,14 +1,25 @@
-#########################################
-# Imports 
-#########################################
 # Database
 from django.db import models
+from django.db.models import Q, F, Count
+from django.db.models.functions import TruncDay
+
+# Storage
+from django.core.files.storage import FileSystemStorage
+
 # Auth
 from django.contrib.auth.models import AbstractUser
 
+# Language
+from nltk.tokenize import word_tokenize
+from nltk.tokenize.util import align_tokens
+from wordfreq import word_frequency
 
-class User(AbstractUser):
-    pass
+# Learning
+import ebisu
+
+# Other
+from django.conf import settings
+from django.utils.html import escape
 
 
 class Language(models.Model):
@@ -20,5 +31,118 @@ class Language(models.Model):
         return self.name
 
 
+class User(AbstractUser):
+    languages = models.ManyToManyField(Language, related_name="speakers")
+
+
+class Sentence(models.Model):
+    lang = models.ForeignKey(Language, related_name="sentences", on_delete=models.CASCADE)
+
+    text = models.TextField()
+
+    audio = models.FileField(storage=FileSystemStorage(location="data", base_url="/data"), null=True, blank=True)
+    translations = models.ManyToManyField("Sentence", blank=True, related_name="translation_of")
+
+    def __str__(self):
+        return self.text
+
+    @property
+    def tokens(self):
+        return word_tokenize(self.text, self.lang.name)
+
+    @property
+    def spans(self):
+        try:
+            return align_tokens(self.tokens, self.text)
+        except ValueError:
+            return align_tokens([tok.replace("''", "\"").replace("``", "\"") for tok in self.tokens], self.text)
+
+
+class TaskQuerySet(models.QuerySet):
+    def random(self, n):
+        count = self.aggregate(count=Count("pk"))["count"]
+        return [self[random.randint(0, count-1)] for _ in range(n)]
+
+
+class Task(models.Model):
+    sentence = models.ForeignKey(Sentence, on_delete=models.CASCADE, related_name="tasks")
+    hidden = models.PositiveSmallIntegerField()
+
+    @property
+    def before(self):
+        return self.sentence.text[:self.sentence.spans[self.hidden][0]]
+
+    @property
+    def after(self):
+        return self.sentence.text[self.sentence.spans[self.hidden][1]:]
+
+
+class UserTaskProgressQuerySet(models.QuerySet):
+    def with_scheduled_review(self):
+        return self.annotate(scheduled_review=models.ExpressionWrapper(
+                F("last_review") + F("interval"),
+                output_field=models.DateTimeField()
+            ))
+
+    def with_scheduled_day(self):
+        return self.annotate(scheduled_day=models.ExpressionWrapper(
+                TruncDay(F("last_review") + F("interval")),
+                output_field=models.DateTimeField()
+            ))
+
+
+class UserTaskProgress(models.Model):
+    # Basic information
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="task_progress")
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="user_progress")
+    last_review = models.DateTimeField(auto_now=True)
+
+    # Spaced repetition parameters
+    alpha = models.FloatField(default=3.0)
+    beta = models.FloatField(default=3.0)
+    interval = models.DurationField(null=True) # hours
+
+    def attempt(self, success, time=None, save=True):
+        if time is None:
+            time = timezone.now()
+
+        if self.last_review is None:
+            self.interval = timedelta(hours=settings.INITIAL_INTERVAL[success])
+        else:
+            model = (self.alpha, self.beta, duration_to_hours(self.interval))
+
+            elapsed = duration_to_hours(time-self.last_review)
+            new_model = ebisu.updateRecall(model, success, 1, elapsed)
+
+            self.alpha = new_model[0]
+            self.beta = new_model[1]
+            self.interval = timedelta(hours=new_model[2])
+
+        self.last_review = time
+        if save:
+            self.save()
+
+    def predict(self, time=None, exact=False):
+        if self.last_review is None or self.interval is None:
+            if exact:
+                return 0.
+            else:
+                return float("-inf")
+
+        if time is None:
+            time = timezone.now()
+
+        elapsed = duration_to_hours(time-self.last_review)
+        model = (self.alpha, self.beta, duration_to_hours(self.interval))
+        return ebisu.predictRecall(model, elapsed, exact=exact)
+
+    @property
+    def next_review(self):
+        if self.last_review is None or self.interval is None:
+            return None
+        return self.last_review + self.interval
+
+
 class Course(models.Model):
-	tasks = models.ManyToManyField(Task, related_name="courses")
+    language = models.ForeignKey(Language, related_name="courses", on_delete=models.CASCADE)
+    tasks = models.ManyToManyField(Task, related_name="courses")
